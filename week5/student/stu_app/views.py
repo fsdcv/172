e2e62e42673b33dafb996e67e5747e38441a5364
fsdcv <fsd.cv2.6th@gmail.com>
2024-06-18 14:22:06 +0530

    adding alert box

--- week5/student/stu_app/views.py
+++ week5/student/stu_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from stu_app.models import student, course
+from django.http import JsonResponse
 
 def home(request):
     return render(request, 'home.html')
@@ -13,21 +14,46 @@ def courselist(request):
     courses = course.objects.all()
     return render(request, 'courselist.html', {'course_list': courses})
 
+# def register(request):
+#     if request.method == "POST":
+#         sid = request.POST.get("student")
+#         cid = request.POST.get("course")
+#         student_obj = student.objects.get(id=sid)
+#         course_obj = course.objects.get(id=cid)
+        
+#         # Checking if the student is already enrolled in the course
+#         if student_obj.courses.filter(id=cid).exists():
+#             resp = "<h1>Student with USN %s has already enrolled for the %s</h1>" % (student_obj.usn, course_obj.courseCode)
+#             return HttpResponse(resp)
+        
+#         student_obj.courses.add(course_obj)
+#         resp = "<h1>Student with USN %s successfully enrolled for the course with course code %s</h1>" % (student_obj.usn, course_obj.courseCode)
+#         return HttpResponse(resp)
+    
+#     else:
+#         students_list = student.objects.all()
+#         courses_list = course.objects.all()
+#         return render(request, 'register.html', {'student_list': students_list, 'course_list': courses_list})
+
 def register(request):
     if request.method == "POST":
         sid = request.POST.get("student")
         cid = request.POST.get("course")
-        student_obj = student.objects.get(id=sid)
-        course_obj = course.objects.get(id=cid)
+        try:
+            student_obj = student.objects.get(id=sid)
+            course_obj = course.objects.get(id=cid)
+            
+            # Checking if the student is already enrolled in the course
+            if student_obj.courses.filter(id=cid).exists():
+                message = "Student with USN %s has already enrolled for the %s" % (student_obj.usn, course_obj.courseCode)
+                return JsonResponse({'message': message, 'status': 'error'})
+            
+            student_obj.courses.add(course_obj)
+            message = "Student with USN %s successfully enrolled for the course with course code %s" % (student_obj.usn, course_obj.courseCode)
+            return JsonResponse({'message': message, 'status': 'success'})
         
-        # Checking if the student is already enrolled in the course
-        if student_obj.courses.filter(id=cid).exists():
-            resp = "<h1>Student with USN %s has already enrolled for the %s</h1>" % (student_obj.usn, course_obj.courseCode)
-            return HttpResponse(resp)
-        
-        student_obj.courses.add(course_obj)
-        resp = "<h1>Student with USN %s successfully enrolled for the course with course code %s</h1>" % (student_obj.usn, course_obj.courseCode)
-        return HttpResponse(resp)
+        except student.DoesNotExist or course.DoesNotExist:
+            return JsonResponse({'message': 'Invalid student or course ID', 'status': 'error'})
     
     else:
         students_list = student.objects.all()
